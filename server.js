2dd006aa85bb6293b2def9b4ae7d802d16b8f05f
McBlanco <caceresmarcelo89@gmail.com>
2016-10-18 18:54:06 -0300

    Ahora se lee la respuesta de la petición desde un fichero HTML

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 var http = require('http');
+var fs = require('fs');
 
 var hostname = '127.0.0.1';
 var port = 4000;
 
 var server = http.createServer(function(req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hola Mundo</h1>');
+	
+	fs.readFile('./default.html', 'utf8', function(error, data) {
+		if (error) {
+			return console.log(error);
+		};
+		console.log('Se leyó el fichero "default.html"');
+		res.statusCode = 200;
+		res.setHeader('Content-Type', 'text/html');
+		res.end(data);
+	});
 });
 
 server.listen(port, hostname, function() {
