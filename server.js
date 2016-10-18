310c1d697977ac349ba9822e1a4ab5fbfa55a100
McBlanco <caceresmarcelo89@gmail.com>
2016-10-18 19:21:05 -0300

    Se agregó un evento que se dispara luego de leer un fichero durante la petición del navegador

--- server.js
+++ server.js
@@ -1,22 +1,32 @@
 var http = require('http');
 var fs = require('fs');
+var events = require('events');
 
 var hostname = '127.0.0.1';
 var port = 4000;
 
+var emitter = new events.EventEmitter();
+
 var server = http.createServer(function(req, res) {
 	
 	fs.readFile('./default.html', 'utf8', function(error, data) {
-		if (error) {
+		if (error)
 			return console.log(error);
-		};
-		console.log('Se leyó el fichero "default.html"');
-		res.statusCode = 200;
+		
+		emitter.emit('fileReaded', data);
+		
 		res.setHeader('Content-Type', 'text/html');
+		res.statusCode = 200;
 		res.end(data);
 	});
+	
 });
 
 server.listen(port, hostname, function() {
   console.log('El servidor se está ejecutando en http://' + hostname + ':' + port);
+});
+
+emitter.on('fileReaded', function(data) {
+	console.log('Se leyó el fichero "default.html"');
+	
 });
